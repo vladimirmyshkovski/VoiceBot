b6a6114facf4c12a977a9ef1d75269f4ef06828a
narnikgamarnikus <narnikgamarnikus@gmail.com>
2017-07-13 21:47:01 +0300

    added requirements and update.sh

--- main.py
+++ main.py
@@ -31,6 +31,6 @@ async def feed(request, ws):
 if __name__ == "__main__":
     app.run(
     	host="0.0.0.0",
-    	port=8000,
+    	port=8080,
     	debug=True
     )
--- update.sh
+++ update.sh
@@ -1,3 +1,5 @@
 git pull
+/var/www/VoiceBot/venv/bin/activate 
+pip install -r requirements/base.txt
 systemctl restart voicebot
 systemctl status voicebot
