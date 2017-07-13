d44cf098b8e428fa0177c007470842ff9202d85d
narnikgamarnikus <narnikgamarnikus@gmail.com>
2017-07-13 23:36:46 +0300

    added static folder and text to Watson speach :)

--- main.py
+++ main.py
@@ -5,10 +5,17 @@ from bot import bot, DEFAULT_SESSION_ID
 from chatterbot.conversation import Statement
 from chatterbot.conversation import Response
 from os.path import join, dirname, realpath
+from text_to_speach import text_to_speach
+import string
+import random
+
+
+def generator(size=36, chars=string.ascii_uppercase + string.digits + string.ascii_lowercase):
+    return ''.join(random.choice(chars) for _ in range(size))
 
 
 app = Sanic()
-
+app.static('/resources', './resources')
 
 @app.route("/")
 async def test(request):
@@ -22,11 +29,14 @@ async def feed(request, ws):
         #statement, response = bot.generate_response(input_statement, DEFAULT_SESSION_ID)
         data = await ws.recv()
         bot_input = bot.get_response(data)
-        z = Response(data)
-        print('RESPONSE: ' + str(z))
-        print('STATEMENT: ' + str(Statement(data)))
-        print(bot_input.serialize()['text'])
+        #z = Response(data)
+        #print('RESPONSE: ' + str(z))
+        #print('STATEMENT: ' + str(Statement(data)))
+        #print(bot_input.serialize()['text'])
+        filename = generator()
+        text_to_speach(bot_input.serialize()['text'], 'text')
         await ws.send(bot_input.serialize()['text'])
+        #await file(join(dirname(__file__),'resources/text.wav'.format(filename)))
     	
 
 if __name__ == "__main__":
---
+++ text_to_speach.py
@@ -0,0 +1,34 @@
+# coding=utf-8
+import ujson as json
+from os.path import join, dirname, realpath
+from watson_developer_cloud import TextToSpeechV1
+#from ..settings import text_to_speech as t
+#from ..settings import UPLOAD_FOLDER
+#text_to_speech = TextToSpeechV1(
+#	username='e7ce4e67-f26d-4d85-afde-56dd9816e67d',
+#	password='Ag7GeK1iX6Nj',
+#	x_watson_learning_opt_out=True)  # Optional flag
+
+text_to_speech = TextToSpeechV1(
+	username='e7ce4e67-f26d-4d85-afde-56dd9816e67d',
+	password='Ag7GeK1iX6Nj',
+	x_watson_learning_opt_out=True)
+
+def text_to_speach(text, filename):
+	#print(json.dumps(s.text_to_speech.voices(), indent=2))
+	#print(join(dirname(__file__)-3))
+	#print(os.path.join(os.path.dirname(__file__),'/static/resources/'))
+	#s = join(dirname(__file__),'/resources/')
+	#print(join(UPLOAD_FOLDER),, '/static/resources')
+	with open(join(dirname(__file__),'resources/{}.wav'.format(filename)),
+	          'wb') as audio_file:
+	    audio_file.write(
+	        text_to_speech.synthesize(str(text), accept='audio/wav',
+	                                  voice="en-US_AllisonVoice"))
+	#print(
+	#    json.dumps(text_to_speech.pronunciation(
+	#        'Watson', pronunciation_format='spr'), indent=2))
+
+	#print(json.dumps(text_to_speech.customizations(), indent=2))
+
+
