795d00f1a2ab366008d259a2764df516a9d08e4e
narnikgamarnikus <narnikgamarnikus@gmail.com>
2017-07-16 07:48:25 +0300

    added requests, and connect this chatbot to the Alice2

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from os.path import join, dirname, realpath
 from text_to_speach import text_to_speach
 import string
 import random
-
+import requests
 
 def generator(size=36, chars=string.ascii_uppercase + string.digits + string.ascii_lowercase):
     return ''.join(random.choice(chars) for _ in range(size))
@@ -27,15 +27,20 @@ async def feed(request, ws):
     while True:
         #input_statement = bot.input.process_input_statement(data)
         #statement, response = bot.generate_response(input_statement, DEFAULT_SESSION_ID)
-        data = await ws.recv()
-        bot_input = bot.get_response(data)
+        question = await ws.recv()
+        #bot_input = bot.get_response(data)
         #z = Response(data)
         #print('RESPONSE: ' + str(z))
         #print('STATEMENT: ' + str(Statement(data)))
         #print(bot_input.serialize()['text'])
         #filename = generator()
         #text_to_speach(bot_input.serialize()['text'], 'text')
-        await ws.send(bot_input.serialize()['text'])
+        data = {
+            "question": question,
+            "sessionid": "1234567890"
+        }
+        r = requests.get('http://localhost:5000/api/v1.0/ask', data)
+        await ws.send(r.json()['response']['answer'])#bot_input.serialize()['text'])
         #await file(join(dirname(__file__),'resources/text.wav'.format(filename)))
     	
 @app.route("/train")
