7153c6224f5385fb45d7f7789b32f130eb54dd6f
narnikgamarnikus <narnikgamarnikus@gmail.com>
2017-07-27 07:21:12 +0300

    changed API port

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ async def feed(request, ws):
             "question": question,
             "sessionid": request['session']['sessionid']
         }
-        r = requests.get('http://45.33.53.142:5000/api/v1.0/ask', data)
+        r = requests.get('http://99.253.55.94:5000/api/v1.0/ask', data)
         if r.status_code is not 200:
             answer = "Something went wrong"
         else:
